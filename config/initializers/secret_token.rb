c32fc4075817d23e575e27c0c46c9bde8f3c0423
Rishi Dhanaraj <rishid97@gmail.com>
2014-01-13 17:43:00 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoyalClothing::Application.config.secret_key_base = 'b85fd33b6f85873d0bae5f1d6686be1dcbeaa2bb9f50d5b3dc48fe443877bdb0e1aa784c6f81fda34cd73b7423e7461d7fa18f5564b608ec9c6e7be07cf31bc9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RoyalClothing::Application.config.session_store :cookie_store, key: '_royal_clothing_session'
